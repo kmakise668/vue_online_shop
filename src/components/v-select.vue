4a65a26bfd0e6e4c74b6accc70a1f883b977dd20
kmakise668 <kmakise668@gmail.com>
2023-07-13 13:09:51 +0600

    text-color

--- src/components/v-select.vue
+++ src/components/v-select.vue
@@ -77,14 +77,14 @@ export default {
                     <ListboxOptions class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                         <ListboxOption v-slot="{ active, selected }" v-for="option in options"  :key="option.value" @click="selectOption(option)" :value="option" as="template">
                             <li :class="[
-                  active ? 'bg-rose-100 text-rose-900' : 'text-gray-900',
+                  active ? 'bg-green-100 text-green-900' : 'text-gray-900',
                   'relative cursor-pointer select-none  py-2 pl-10 pr-4',
                 ]">
                                 <span :class="[
                     selected ? 'font-medium' : 'font-normal',
                     'block truncate',
                   ]">{{ option.name }}</span>
-                                <span v-if="selected" class="absolute inset-y-0 left-0 flex items-center pl-3 text-rose-600">
+                                <span v-if="selected" class="absolute inset-y-0 left-0 flex items-center pl-3 text-green-600">
                                     <CheckIcon class="h-5 w-5" aria-hidden="true" />
                                 </span>
                             </li>
